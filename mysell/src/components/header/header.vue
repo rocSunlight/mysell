a4da8b22d169ef6c8095fd54a18593aa6671ce6b
wojiaohpx <1659253001@qq.com>
2017-02-13 23:23:17 +0800

    123
    123

--- mysell/src/components/header/header.vue
+++ mysell/src/components/header/header.vue
@@ -15,18 +15,50 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}} 个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span><i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="bg-img">
+      <img :src="seller.avatar" :alt="seller.name" width="100%" height="100%">
+    </div>
+    <div class="detail" v-show="detailShow">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <div class="star-wrapper">
+            <v-star :size="48" :score="seller.score"></v-star>
+          </div>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close" @click="hideDetail"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import star from 'components/star/star'
   export default{
     // prop是父组件用来传递数据的一个自定义属性。子组件需要显式地用 props 选项声明 “prop”
+    data() {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true
+      },
+      hideDetail() {
+        this.detailShow = false
+      }
+    },
     props: {
       seller: {
         type: Object
@@ -34,8 +66,11 @@
     },
     created() {
       this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+    },
+    // 注册组件
+    components: {
+      'v-star': star
     }
-
   }
 </script>
 
@@ -44,7 +79,9 @@
 
   .header
     color #fff
-    background #999
+    position relative
+    background rgba(7,17,27,0.5)
+    overflow hidden
     .content-wrapper
       position relative
       padding 24px 12px 18px 24px
@@ -119,6 +156,74 @@
           margin-left 2px
           line-height 24px
           font-size 10px
+    .bulletin-wrapper
+      line-height 28px
+      height 28px
+      padding 0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      position relative
+      background rgba(7,17,27,0.2)
+      .bulletin-title
+        display inline-block
+        vertical-align top
+        margin-top 8px
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+        vertical-align top
+        font-size 10px
+        margin 0 4px
+      .icon-keyboard_arrow_right
+        position absolute
+        right 12px
+        top 8px
+        font-size 10px
 
+    .bg-img
+      position absolute
+      top 0
+      left 0
+      width 100%
+      height 100%
+      z-index -1
+      filter blur(10px)
+      -webkit-filter blur(10px)
 
+    .detail
+      position fixed
+      z-index 100
+      width 100%
+      height 100%
+      overflow auto
+      top 0
+      left 0
+      background rgba(7,17,27,0.8)
+      .detail-wrapper
+        min-height 100%
+        width 100%
+        .detail-main
+          margin-top 64px
+          padding-bottom 64px
+          .name
+            line-height 16px
+            text-align center
+            font-size 16px
+            font-weight 700
+
+          .star-wrapper
+            margin-top 18px
+            padding 2px 0
+            text-align center
+      .detail-close
+        position relative
+        width 32px
+        height 32px
+        margin -64px auto 0 auto
+        clear both
+        font-size 32px
 </style>
